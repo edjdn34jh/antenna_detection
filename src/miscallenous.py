7bb6ee2d4523d2793573b94429b7b5a79b297e64
edjdn34jh <github.com.boundledd465@passmail.net>
2026-01-27 11:28:57 +0100

    Add comments to miscallenous.py and tiledownload.py

--- src/miscallenous.py
+++ src/miscallenous.py
@@ -1,10 +1,25 @@
+######################################################################################################
+########################## Some miscallenous function used in the  ###################################
+##########################          tile_download script           ###################################
+######################################################################################################
+
+
+
 import rasterio
 from rasterio.mask import mask
 from shapely.geometry import box
 import geopandas as gpd
 
 
+
 def clip_tiff(tiff_path, geometry, output_path):
+    """
+    Clip a tiff according to a geometry
+    
+    :param tiff_path: The path to the input tiff
+    :param geometry: The geometry to clip the tiff
+    :param output_path: The path to save the clipped tiff
+    """
     with rasterio.open(tiff_path) as src:
 
         out_image, out_transform = mask(
@@ -24,7 +39,16 @@ def clip_tiff(tiff_path, geometry, output_path):
             dest.write(out_image)
 
 
+
+
 def rectangle_around_point(point, half_size):
+    """
+    Given a point and dimension, create a square geometry centered around that point
+    
+    :param point: The point around which to center the geometry
+    :param half_size: The halfe size of the square
+    """
+
     x, y = point.x, point.y
     return box(
         x - half_size,
--- src/tile_download.py
+++ src/tile_download.py
@@ -1,10 +1,16 @@
+######################################################################################################
+########################## Python script to automatically download pcrs .tif #########################
+##########################    tiles and clip them around selected antenna    #########################
+######################################################################################################
+
+
 import geopandas as gpd
 import requests
 import pathlib
 from miscallenous import clip_tiff, rectangle_around_point
 
 # Load the pcrs tiles data
-dalles_pcrs_gdf = gpd.read_file("./dalles.geojson")
+pcrs_tiles_gdf = gpd.read_file("./dalles.geojson")
 
 
 # Load the antenna data
@@ -12,55 +18,61 @@ antenna_gdf = gpd.read_file("./antenne_reproj2154.gpkg")
 
 
 # Convert to same crs
-dalles_pcrs_gdf = dalles_pcrs_gdf.to_crs(antenna_gdf.crs)
+pcrs_tiles_gdf = pcrs_tiles_gdf.to_crs(antenna_gdf.crs)
 
-# 
-antenna_dalles_join_gdf = gpd.sjoin(
+# Join the antenna and tile dataframe
+# The resulting dataframe is composed of all the antennas and for each, the tile that it seats on
+antenna_tiles_join_gdf = gpd.sjoin(
     antenna_gdf,
-    dalles_pcrs_gdf,
+    pcrs_tiles_gdf,
     how="left",
     predicate="within"
 )
 
 
-
+# The url where the pcrs tiles are stored
 root_url = "https://nas-g2f.g2f-pub.smsn.fr/bloc_"
 sub_dirs = ["a", "b", "c", "d", "e", "f", "gn", "gs", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"]
 
 
-all_dalle = list(set(antenna_dalles_join_gdf["nomImage"].to_list()))
+# List of all uniques tiles where there are antennas
+all_tiles = list(set(antenna_tiles_join_gdf["nomImage"].to_list()))
 
-dalle_name = all_dalle[1]
+# The tile we want to download
+tile_name = all_tiles[1]
 
-points = list(set((antenna_dalles_join_gdf[antenna_dalles_join_gdf["nomImage"] == dalle_name])["geometry"].tolist()))
+# List of all unique antennas that seats within the given tile
+points = list(set((antenna_tiles_join_gdf[antenna_tiles_join_gdf["nomImage"] == tile_name])["geometry"].tolist()))
 
 
+# Download the chosen tile
 for dir in sub_dirs:
 
-    # antenna_dalles_join_gdf[antenna_dalles_join_gdf["nomImage"] == dalle_name]
-
-    url = root_url + dir + '/' + dalle_name
+    url = root_url + dir + '/' + tile_name
 
     r = requests.get(url)
 
     if r.status_code == 200:
-        with open(f"./pcrs_images/{dalle_name}", "wb") as f:
+        with open(f"./pcrs_images/{tile_name}", "wb") as f:
 
             f.write(r.content)
 
             print(f'Downloaded {url}')
 
+
+# Clip a 30x30m square around the points in the tiles and save them as tif files
 for i, point in enumerate(points):
 
     rect = rectangle_around_point(point, 15)
 
     clip_tiff(
-        tiff_path=f"./pcrs_images/{dalle_name}",
+        tiff_path=f"./pcrs_images/{tile_name}",
         geometry=rect,
         output_path=f"./antenna_images/{point}.tif"
     )
 
-file_to_rem = pathlib.Path(f"./pcrs_images/{dalle_name}")
+# Remove the dowloaded tile to save memory space
+file_to_rem = pathlib.Path(f"./pcrs_images/{tile_name}")
 file_to_rem.unlink()
 
 
